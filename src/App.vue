e6dd7bad73d44da9f41c0aa92d2b194e2e6bc6e8
Jesus Berdugo <berdugopertuz@gmail.com>
2023-12-11 08:33:06 -0500

    chore: setup auto import components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts" >
 const test = ref(0);
-const router = useRouter();
+const router = useRouter()
 </script>
 
 <template>
   <main class="flex justify-center items-center w-screen h-screen bg-red-500">
     <h1>Hello World!</h1>
+    <Button>Click me</Button>
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      imports: [
-        'vue',
-        'vue-router'
-      ],
+      imports: ["vue", "vue-router"],
+    }),
+    Components({
+      dirs: ["./src/components"],
     }),
   ],
   resolve: {
